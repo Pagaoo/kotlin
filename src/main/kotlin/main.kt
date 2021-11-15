bcb94956cca6b9d03a86a41823e43dae9c92b344
Gabriel Paganini <gabrielpaganinibarroso@hotmail.com>
2021-11-15 14:23:42 -0300

    primeiros passo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,27 @@
+// var é uma palavra reservada que remete a um dado que pode ser mudado durante o programa.
+// val é uma palavra reservada que remete a um dado imutavel.
+
 fun main() {
     println("Hello World!");
 
     val nome = "Gabriel";
     val idade = 22;
 
-    println(nome);
-    println(idade);
+    //println(nome);
+    //println(idade);
+
+    val frase = "Kotlin é um linguagem"
+    val caract = "show!"
+    println("$frase $caract")
+
+    // Um bom jeito de setar strings muito grandes
+    val str = """adskladskadskads
+        |adsopadsopadkosads
+        |adkosadkopskopads
+        |akodsopadkskopads
+        |koadskopadskoadsoads
+        |asdadskopadskopads
+    """.trimMargin()
+
+    println("${str.length}")
 }
