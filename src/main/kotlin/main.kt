a89f3dff1a0dbd566e1e258c24b4fab837e338a6
Gabriel Paganini <gabrielpaganinibarroso@hotmail.com>
2021-11-15 14:36:23 -0300

    basico de funções

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,24 @@
 // var é uma palavra reservada que remete a um dado que pode ser mudado durante o programa.
 // val é uma palavra reservada que remete a um dado imutavel.
 
+fun calcBonus(a: Int, b: Int, c: Int){
+    println("Meu bônus é: ${a + b * c * 2}")
+}
+
+fun hello(nome: String) : String{
+    return "Hello $nome"
+}
+
 fun main() {
     println("Hello World!");
 
-    val nome = "Gabriel";
-    val idade = 22;
+    val nome = "Gabriel"
+    val a = 10
+    val b = 20
+    val c = 30
 
-    //println(nome);
-    //println(idade);
+    calcBonus(a,b,c)
+    println(hello(nome))
 
     val frase = "Kotlin é um linguagem"
     val caract = "show!"
